0b2e700ec9174f3346eecaf8d495bf052948d148
Mikolaj <mik@fabric-analytics.com>
2025-03-20 11:24:38 +0000

    progressing

--- scripts.js
+++ scripts.js
@@ -32,7 +32,8 @@ map.setMaxBounds(gbBounds);
 //Add a tile layer (background map)
 var blackMap = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
-    subdomains: 'abcd'
+    subdomains: 'abcd',
+    opacity: 0
 });
 blackMap.addTo(map);
 
